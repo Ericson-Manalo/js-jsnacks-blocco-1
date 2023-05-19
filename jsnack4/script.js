ed3532c0cda1e5f3e2713cb25e2ca464ef8fb130
Ericson-Manalo <ericmana98@gmail.com>
2023-05-19 11:14:52 +0200

    corrected the excercise with prof's lc

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,10 +1,17 @@
 /* Chiedi un numero di 4 cifre all’utente
 e calcola la somma di tutte le cifre che compongono il numero. */
 
-const firstNum = parseInt(prompt ('Insert a number'));
-const secondNum = parseInt(prompt ('Insert another number'));
-const thirdNum = parseInt(prompt ('Insert another number'));
-const fourthNum = parseInt(prompt ('Insert a last number'));
 
-let result = firstNum.value + secondNum.value + thirdNum.value + fourthNum.value;
-console.log(result);
+let numberOfFour = prompt('Inserisci un numero di 4 cifre');
+
+if (!(numberOfFour > 999 && numberOfFour < 9999)){ 
+    numberOfFour = prompt('Inserisci un numero di 4 cifre');
+}
+
+let sum = 0;
+
+for ( let i = 0; i < numberOfFour.length; i++){
+    sum += parseInt(numberOfFour.charAt(i)); 
+}
+
+console.log(sum);
